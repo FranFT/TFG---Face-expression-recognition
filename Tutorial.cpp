e98f17b64d802ff4b06a00539d5d2a2a4a98b18e
FranFT - Fran Fajardo <frannnlol@gmail.com>
2015-10-26 10:55:38 +0100

    Prueba de detección de fronteras con el detector de Canny

--- Tutorial.cpp
+++ Tutorial.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <opencv2\opencv.hpp>
+#include <vector>
 
 using namespace cv;
 using namespace std;
@@ -153,10 +154,43 @@ void pintaMI(const vector<vector<Mat> > &imagenes_solucion, char* nombre = "solu
 	pintaI(solucion, nombre);
 }
 
+//// Detección de características
+// Detector de bordes de Canny.
+// http://docs.opencv.org/doc/tutorials/imgproc/imgtrans/canny_detector/canny_detector.html
+//
+Mat CannyThreshold(const Mat& img, double low_threshold, int kernel_size = 3, int ratio = 3)
+{
+	// Variables necesarias.
+	Mat detected_edges;
+
+	// Alisamos la imagen reduciendo el ruido y la guardamos en 'detected_edges'.
+	blur(img, detected_edges, Size(kernel_size, kernel_size));
+
+	// Aplicamos el detector de bordes de Canny
+	// (http://docs.opencv.org/modules/imgproc/doc/feature_detection.html?highlight=canny#canny).
+	Canny(detected_edges, detected_edges, low_threshold, low_threshold*ratio, kernel_size);
+
+	return detected_edges;
+}
 
 int main(){
-	vector<Mat> imágenes_solucion();
+	vector<Mat> imagenes_solucion;
+
+	// Leo imagen.
 	Mat prueba = leeimagen("yalefaces/subject01.png");
+
+	// La añado al vector de imágenes solución.
+	imagenes_solucion.push_back(prueba);
+
+	// Aplico el detector de fronteras de Canny.
+	prueba = CannyThreshold(prueba, 40);
+
+	// Añado la imagen al vector de imágenes solución.
+	imagenes_solucion.push_back(prueba);
+
+	// Pinto las imágenes del vector.
+	pintaMI(imagenes_solucion);
+
 	
 	return 0;
 }
