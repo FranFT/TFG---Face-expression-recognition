bfcad30821fa36523f14ba802bdd7e63c2fec916
Francisco José Fajardo Toril <frannnlol@gmail.com>
2016-11-21 14:01:21 +0100

    Añadido ejemplo para detectar manos con la variante de la libería STASM simplificada. Ademas se han modificado ambas aplicaciones de detección de manos para capturar los frames donde se producen detecciones.

--- apps/detectarManosSimplificado.sh
+++ apps/detectarManosSimplificado.sh
@@ -6,4 +6,7 @@ source ../data/info.sh
 # Descripción: Genera la aplicación para el reconocimiento de manos.#
 #####################################################################
 
+INDICE=`basename "$0"`
+INDICE="${INDICE%.*}"
 
+./$BUILD_APP $INDICE TEST
--- lib/STASM_handsSimplificado/tasm/landtab/landtab_hands.h
+++ lib/STASM_handsSimplificado/tasm/landtab/landtab_hands.h
@@ -1,12 +1,12 @@
-#ifndef STASM_LANDTAB_HANDS23_H
-#define STASM_LANDTAB_HANDS23_H
+#ifndef STASM_LANDTAB_HANDS11_H
+#define STASM_LANDTAB_HANDS11_H
 
 // Note that we use classical 1D descriptors on all points (no HATS)
 // because all points are on edges.  HATs would probably work as well
 // but I haven't tried them here (to use a HAT descriptor at a point,
 // set the AT_Hat bit in the "bits" field).
 
-static const LANDMARK_INFO LANDMARK_INFO_TAB[23] =
+static const LANDMARK_INFO LANDMARK_INFO_TAB[11] =
 {  // par pre next weight bits
 	{ -1, -1,  -1,     1,   0 }, //0
 	{ -1, -1,  -1,     1,   0 }, //1
@@ -19,18 +19,6 @@ static const LANDMARK_INFO LANDMARK_INFO_TAB[23] =
 	{ -1, -1,  -1,     1,   0 }, //8
 	{ -1, -1,  -1,     1,   0 }, //9
 	{ -1, -1,  -1,     1,   0 }, //10
-	{ -1, -1,  -1,     1,   0 }, //11
-	{ -1, -1,  -1,     1,   0 }, //12
-	{ -1, -1,  -1,     1,   0 }, //13
-	{ -1, -1,  -1,     1,   0 }, //14
-	{ -1, -1,  -1,     1,   0 }, //15
-	{ -1, -1,  -1,     1,   0 }, //16
-	{ -1, -1,  -1,     1,   0 }, //17
-	{ -1, -1,  -1,     1,   0 }, //18
-	{ -1, -1,  -1,     1,   0 }, //19
-	{ -1, -1,  -1,     1,   0 }, //20
-	{ -1, -1,  -1,     1,   0 }, //21
-	{ -1, -1,  -1,     1,   0 }, //22
 };
 
-#endif //STASM_LANDTAB_HANDS23_H
+#endif //STASM_LANDTAB_HANDS11_H
