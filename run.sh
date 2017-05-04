c7b9d7767394f5a92e6a41166192540ddfa6fac2
Francisco José Fajardo Toril <frannnlol@gmail.com>
2017-05-04 18:26:18 +0200

    Creando imagen media

--- run.sh
+++ run.sh
@@ -8,20 +8,14 @@ TEST_DB_NAME="kdef_test_lmdb"
 log_msg "Preprocessing images"
 cd $PREPROC_DIR && ./run.sh $1
 
-if [ ! -d "../data/kdef/$TRAINING_DB_NAME" ];then
+if [ ! -d "../data/kdef/$TRAINING_DB_NAME" ] || [ ! -d "../data/kdef/$TEST_DB_NAME" ];then
   log_msg "Creating data base"
   rm -r ../data/kdef/*
   cd build
   ../../$CAFFE_TOOLS/convert_imageset training/ trainingListFile.txt ../../data/kdef/$TRAINING_DB_NAME
   ../../$CAFFE_TOOLS/convert_imageset test/ testListFile.txt ../../data/kdef/$TEST_DB_NAME
-  cd ../
-elif [ ! -d "../data/kdef/$TEST_DB_NAME" ];then
-  log_msg "Creating data base"
-  rm -r ../data/kdef/*
-  cd build
-  ../../$CAFFE_TOOLS/convert_imageset training/ trainingListFile.txt ../../data/kdef/$TRAINING_DB_NAME
-  ../../$CAFFE_TOOLS/convert_imageset test/ testListFile.txt ../../data/kdef/$TEST_DB_NAME
-  cd ../
+  cd ../../
+  $CAFFE_TOOLS/compute_image_mean -backend=lmdb data/kdef/$TRAINING_DB_NAME data/kdef/mean.binaryproto
 else
   echoG "Data Base being used:"
   echoG "------ '../data/kdef/$TRAINING_DB_NAME'"
