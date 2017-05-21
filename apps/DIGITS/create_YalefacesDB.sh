23df9579c8932fc098f5dc454d3c5a48fda46686
Francisco José Fajardo Toril <frannnlol@gmail.com>
2017-05-21 13:59:08 +0200

    Soporte para NVIDIA DIGITS

---
+++ apps/DIGITS/create_YalefacesDB.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+source configuration.sh
+
+EXPR=(centerlight glasses happy leftlight noglasses normal rightlight sad sleepy surprised wink)
+DB_DIR=yalefaces
+
+rm -r ~/DIGITS/datasets/$DB_DIR
+
+if [ -d ~/DIGITS/datasets/$DB_DIR ]; then
+  exit 1
+fi
+
+# Creating Folder Structure
+if [ -d data-preprocessing/build ]; then
+  rm -r data-preprocessing/build
+fi
+
+mkdir data-preprocessing/build
+mkdir data-preprocessing/build/temp
+for i in ${EXPR[@]}; do
+  mkdir data-preprocessing/build/temp/${i}
+  echo ${i} >> data-preprocessing/build/temp/labels.txt
+done
+
+cd data-preprocessing/build
+cmake .. && make
+./DIGITS_yalefaces
+
+mv temp ~/DIGITS/datasets/$DB_DIR
